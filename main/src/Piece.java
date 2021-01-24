811c537b2d197beb1deb0664967a4dc127db3a45
unknown <jlee3688@gatech.edu>
2021-01-24 16:37:16 -0600

    add Piece class javadocs

--- main/src/Piece.java
+++ main/src/Piece.java
@@ -1,19 +1,30 @@
+/**
+ * This is class that represents Piece.
+ * @version 1.0
+ */
 public class Piece {
 
     private String pieceName;
-    private int numOfMove;
-    private String movementCode;
-    private int teamNumber;
-    private boolean upPawn;
+    private int numOfMove; //number of movement. (for special rules, such as En Passant, Castling)
+    private String movementCode; //represent movement. (P = Pawn, R = Rook, K = Knight, B = bishop, Q = Queen, K = King)
+    private int teamNumber; //represent teamNumber (for 1vs1vs1vs1)
+    private boolean upPawn; //direction of pawn (only for pawn object, else -> false all)
     private boolean downPawn;
     private boolean rightPawn;
     private boolean leftPawn;
 
-    public Piece(String pieceName, int numOfMove, String movementCode
-            , int teamNumber, String pawnDirection) {
+    /**
+     * Constructor for Piece.
+     * @param pieceName name of Piece (e.g pawn, rook, knight, etc)
+     * @param numOfMove number of movement (see above)
+     * @param movementCode movementCode (see above) 
+     * @param teamNumber represent teamNumber (see above) 
+     * @param pawnDirection direction of pawn (see above)
+     */
+    public Piece(String pieceName, int numOfMove, int teamNumber, String pawnDirection) {
         this.pieceName = pieceName;
         this.numOfMove = numOfMove;
-        this.movementCode = movementCode;
+        this.movementCode = "";
         this.teamNumber = teamNumber;
         this.upPawn = false;
         this.downPawn = false;
@@ -30,6 +41,12 @@ public class Piece {
         }
     }
 
+    /**
+     * Constructor for Piece (we mostly use)
+     * @param pieceName name of Piece (e.g pawn, rook, knight, etc)
+     * @param teamNumber represent teamNumber (see above)
+     * @param pawnDirection direction of pawn (see above)
+     */
     public Piece(String pieceName, int teamNumber, String pawnDirection) {
         this(pieceName, 0, "", teamNumber, pawnDirection);
         if (pieceName.equalsIgnoreCase("pawn")) {
@@ -49,6 +66,11 @@ public class Piece {
         }
     }
 
+    /**
+     * This method is designed to implement queening.
+     * @param pieceName The piece that the pawn will change to
+     * @return new Piece
+     */
     public Piece getQueeningPiece(String pieceName) {
         Piece queeningPiece;
         if (pieceName.equalsIgnoreCase("rook")) {
@@ -84,22 +106,41 @@ public class Piece {
         return queeningPiece;
     }
 
+    /**
+     * This method will increase the numberOfMovement.
+     */
     public void increamentNumOfMovement() {
         this.numOfMove++;
     }
 
+    /**
+     * getter for pieceName
+     * @return pieceName
+     */
     public String getPieceName() {
         return pieceName;
     }
 
+    /**
+     * getter for NumOfMove
+     * @return NumOfMove
+     */
     public int getNumOfMove() {
         return numOfMove;
     }
 
+    /**
+     * getter for movementCode
+     * @return movementCode
+     */
     public String getMovementCode() {
         return movementCode;
     }
 
+    /**
+     * getter for pawnDirection
+     * @return pawnDirection
+     */
     public String getPawnDirection() {
         if (upPawn) {
             return "up";
@@ -114,6 +155,10 @@ public class Piece {
         }
     }
 
+    /**
+     * getter for TeamNumber
+     * @return teamNumber
+     */
     public int getTeamNumber() {
         return teamNumber;
     }
